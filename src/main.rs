e15ef1949bdcd4bfa4ef2ad40de0b29a7b5b882d
noharu36 <noharu@noshimaakinoMacBook-Pro.local>
2024-05-29 00:13:22 +0900

    create message

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use tush::run_shell::shell_loop;
+use colored::*;
 
 fn main() {
+    println!(
+        "
+                        ╭╯         ╭╯
+                        ╰╮Smoking ╭╯    {}
+                        ╭╯ TIME ╭╯
+        ▓▓██████████▒ ╭━╯               {} {} {} {} {}🚬
+        ",
+        "Hello tush!".bright_cyan().bold(),
+        "tupakka".bright_red().bold(),
+        "+".bright_white().bold(),
+        "shell".bright_magenta().bold(),
+        "=".bright_white().bold(),
+        "tush".bright_cyan().bold()
+        );
     shell_loop()
 }
